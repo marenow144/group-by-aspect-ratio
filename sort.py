2d55acc089f24a39badfc681fc5a6483ace7501f
Artur Nowakowski <artur.nowakowski@sap.com>
2017-07-26 21:46:01 +0200

    Rounding ratio to 2nd decimal number. Changed way of copying photos

--- sort.py
+++ sort.py
@@ -1,35 +1,37 @@
 from PIL import Image
 import os
 import argparse
+import shutil
 
 def process(input,output):
     for filename in os.listdir(input):
                try:
-                img  = Image.open(os.path.join(input, filename))
+                path = os.path.join(input, filename)
+                img  = Image.open(path)
                 img.verify()
-                img  = Image.open(os.path.join(input, filename))
+                img  = Image.open(path)
                 width, height = img.size
-                aspect_ratio = width / height
-                if aspect_ratio == 4 / 3 or aspect_ratio == 3 / 4:
+                aspect_ratio = round(width / height,2)
+                if aspect_ratio == round(4 / 3,2) or aspect_ratio == round(3 / 4,2):
                   if not os.path.exists(os.path.join(output,'4x3')):
                            os.makedirs(os.path.join(output,'4x3'))
-                  img.save(os.path.join(output,'4x3', filename))
-                elif aspect_ratio == 2 / 3 or aspect_ratio == 3 / 2:
-                    if not os.path.exists(os.path.join(output,'2x3')):
-                           os.makedirs(os.path.join(output,'2x3'))
-                    img.save(os.path.join(output,'2x3', filename))
-                elif aspect_ratio == 16 / 9 or aspect_ratio == 9 / 16:
+                  shutil.copy2(path, os.path.join(output,'4x3'))
+                elif aspect_ratio == round(2 / 3,2) or aspect_ratio == round(3 / 2,2):
+                    if not os.path.exists(os.path.join(output,'3x2')):
+                           os.makedirs(os.path.join(output,'3x2'))
+                    shutil.copy2(path, os.path.join(output,'3x2'))
+                elif aspect_ratio == round(16 / 9,2) or aspect_ratio == round(9 / 16,2):
                     if not os.path.exists(os.path.join(output,'16x9')):
                            os.makedirs(os.path.join(output,'16x9'))
-                    img.save(os.path.join(output,'16x9', filename))
+                    shutil.copy2(path, os.path.join(output,'16x9'))
                 elif aspect_ratio == 1:
                      if not os.path.exists(os.path.join(output,'1x1')):
                            os.makedirs(os.path.join(output,'1x1'))
-                     img.save(os.path.join(output,'1x1', filename))
+                     shutil.copy2(path, os.path.join(output,'1x1'))
                 else:
                     if not os.path.exists(os.path.join(output,'other')):
                            os.makedirs(os.path.join(output,'other'))
-                    img.save(os.path.join(output,'other', filename))
+                    shutil.copy2(path, os.path.join(output,'other'))
                except Exception as err:
                     print("filename: {} has wrong format {}" .format(filename, err))
 
